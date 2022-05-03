046b5279718196981c3fc7ba2d0a31b308ccc952
Bob McNabb <robertmcnabb@gmail.com>
2022-05-03 11:40:40 +0100

    add archive location map, update study map

--- study_area_map.py
+++ study_area_map.py
@@ -3,55 +3,121 @@ import geopandas as gpd
 from shapely.geometry.polygon import Polygon
 from shapely.geometry.multipolygon import MultiPolygon
 import cartopy.crs as ccrs
+import cartopy.feature as cf
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
 def _coords(row):
-    return [row[['lon_min', 'lat_min']].values, row[['lon_max', 'lat_min']].values,
-            row[['lon_max', 'lat_max']].values, row[['lon_min', 'lat_max']].values]
+    return (tuple(row[['lon_min', 'lat_min']].values),
+            tuple(row[['lon_max', 'lat_min']].values),
+            tuple(row[['lon_max', 'lat_max']].values),
+            tuple(row[['lon_min', 'lat_max']].values))
+
+
+# https://stackoverflow.com/a/25628397
+def get_cmap(n, name='hsv'):
+    """
+    Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.
+    """
+    return plt.cm.get_cmap(name, n)
+
 
 plt.ion()
 
 # read the geographic sheet from the excel file
-df_dict = pd.read_excel('data/Review_Historic_Air_Photos.xlsx', sheet_name=['geographic', 'scientific'])
+df = pd.read_csv('data/Review_Historic_Air_Photos.csv')
 
-# extract the publication key from the geographic and scientific tables
-for sheet in df_dict.keys():
-    df_dict[sheet]['Pub Key'] = df_dict[sheet]['Publication Key'].str.extract(r'\(([^()]{8})\)')
-
-    # drop empty rows
-    df_dict[sheet].dropna(how='all', inplace=True)
-
-    # drop the publication key
-    df_dict[sheet].drop(['Publication Key'], axis=1, inplace=True)
-
-df = df_dict['geographic'].set_index('Pub Key').join(df_dict['scientific'].set_index('Pub Key'), rsuffix='_sci')
-df.dropna(subset='lat_min', inplace=True)
+# drop any rows with geometry missing
+df.dropna(subset=['lat_min', 'lat_max', 'lon_min', 'lon_max'], inplace=True)
 
 # add a geometry column
 df['geometry'] = [_coords(row) for ii, row in df.iterrows()]
+
+# get only unique geometry and data types
+df.drop_duplicates(subset=['geometry', 'Type'], inplace=True)
 df['geometry'] = df['geometry'].apply(Polygon)
 
 # only take the papers that are counted as "relevant"
 df = df[df['Relevant'] == 'yes']
 
-study_areas = gpd.GeoDataFrame(df[['Data Type', 'geometry']])
+study_areas = gpd.GeoDataFrame(df[['Key', 'Type', 'Archive Location', 'geometry']])
 study_areas.set_crs(epsg=4326, inplace=True)
 
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.coastlines()
+# reproject to the robinson projection
+study_areas['x'] = study_areas.to_crs("esri:54030")['geometry'].centroid.x
+study_areas['y'] = study_areas.to_crs("esri:54030")['geometry'].centroid.y
 
-alpha = 0.2
+# create a Robinson projection object
+robinson = ccrs.Robinson()
 
-study_areas[study_areas['Data Type'] == 'Satellite'].plot(ax=ax, alpha=alpha, fc='r', ec='k')
-study_areas[study_areas['Data Type'] == 'Aerial'].plot(ax=ax, alpha=alpha, fc='b', ec='k')
-study_areas[study_areas['Data Type'] == 'Mix'].plot(ax=ax, alpha=alpha, fc='k', ec='k')
+# -------------------------------------------------------------------------------------------------------------------
+# create a map that shows the study sites plotted using an individual marker
+study_area_fig = plt.figure(figsize=(18, 10))
+ax = plt.axes(projection=robinson)
+ax.add_feature(cf.BORDERS)
+ax.coastlines(resolution='110m')
+ax.set_ylim(robinson.y_limits)
+ax.set_xlim(robinson.x_limits)
+
+sat = study_areas['Type'] == 'Satellite'
+aer = study_areas['Type'] == 'Aerial'
+ter = study_areas['Type'] == 'Terrestrial'
+
+ax.plot(study_areas.loc[sat, 'x'], study_areas.loc[sat, 'y'], 'bs', markerfacecolor='none', ms=12, label='Satellite')
+ax.plot(study_areas.loc[aer, 'x'], study_areas.loc[aer, 'y'], 'ro', markerfacecolor='none', ms=12, label='Aerial')
+ax.plot(study_areas.loc[ter, 'x'], study_areas.loc[ter, 'y'], 'k^', markerfacecolor='none', ms=12, label='Terrestrial')
+
+# add a legend with the type of data
+ax.legend(fontsize=14, frameon=False, loc='upper right')
+
+full_ext = ax.get_extent()
+study_area_fig.savefig('study_map.png', dpi=300, bbox_inches='tight')
+
+# -------------------------------------------------------------------------------------------------------------------
+# color each symbol according to the type of data and the archive location
+
+archive_fig = plt.figure(figsize=(20, 10))
+ax = plt.axes(projection=robinson)
+ax.add_feature(cf.BORDERS)
+ax.coastlines(resolution='110m')
+ax.set_ylim(robinson.y_limits)
+ax.set_xlim(robinson.x_limits)
+
+# arch_locs = study_areas['Archive Location'].unique()
+archive_count = study_areas.groupby(['Archive Location'])['geometry'].count()
+arch_locs = list(archive_count.sort_values(ascending=False).head().index)
+
+# get a colormap with unique colors for each archive location
+colors = get_cmap(len(arch_locs) + 1, name='Set1')
+
+alpha = 0.4  # set the transparency for the markers
 
 handles = list()
-for fc in ['r', 'b', 'k']:
-    handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=fc, edgecolor='k', alpha=alpha))
 
-labels = ['Satellite', 'Aerial', 'Mix']
+for ii, arch_loc in enumerate(arch_locs):
+    this_sat = (study_areas['Type'] == 'Satellite') & (study_areas['Archive Location'] == arch_loc)
+    this_aer = (study_areas['Type'] == 'Aerial') & (study_areas['Archive Location'] == arch_loc)
+    this_ter = (study_areas['Type'] == 'Terrestrial') & (study_areas['Archive Location'] == arch_loc)
 
-ax.legend(handles, labels, fontsize=14, loc='lower left', frameon=True, framealpha=1)
+    ax.plot(study_areas.loc[this_sat, 'x'], study_areas.loc[this_sat, 'y'], 's', color=colors(ii), alpha=alpha, ms=12)
+    ax.plot(study_areas.loc[this_aer, 'x'], study_areas.loc[this_aer, 'y'], 'o', color=colors(ii), alpha=alpha, ms=12)
+    ax.plot(study_areas.loc[this_ter, 'x'], study_areas.loc[this_ter, 'y'], '^', color=colors(ii), alpha=alpha, ms=12)
+
+    handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors(ii), edgecolor='k', alpha=alpha))
+
+# now add the "other" category
+this_sat = (study_areas['Type'] == 'Satellite') & (~study_areas['Archive Location'].isin(arch_locs))
+this_aer = (study_areas['Type'] == 'Aerial') & (~study_areas['Archive Location'].isin(arch_locs))
+this_ter = (study_areas['Type'] == 'Terrestrial') & (~study_areas['Archive Location'].isin(arch_locs))
+
+ax.plot(study_areas.loc[this_sat, 'x'], study_areas.loc[this_sat, 'y'], 's', color=colors(ii+1), alpha=alpha, ms=12)
+ax.plot(study_areas.loc[this_aer, 'x'], study_areas.loc[this_aer, 'y'], 'o', color=colors(ii+1), alpha=alpha, ms=12)
+ax.plot(study_areas.loc[this_ter, 'x'], study_areas.loc[this_ter, 'y'], '^', color=colors(ii+1), alpha=alpha, ms=12)
+
+handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors(ii+1), edgecolor='k', alpha=alpha))
+arch_locs.append('other/not specified')
+
+ax.legend(handles, arch_locs, fontsize=12, loc='lower left', frameon=True, framealpha=1)
+plt.savefig('archive_locations.png', dpi=300, bbox_inches='tight')
